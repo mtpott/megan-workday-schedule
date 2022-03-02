af1e664ce5fa8f8e5df57dc70f9950610ab4ac08
megan <mtpott23@gmail.com>
2022-03-02 16:09:31 -0700

    added moment.js feature

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,69 @@
+// GIVEN I am using a daily planner to create a schedule
+
+// WHEN I refresh the page
+// THEN the saved events persist
+
+var items = {};
+
+saveButtonEl = document.getElementsByClassName("saveBtn");
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+function showDate() {
+    var dateEl = document.createElement("p");
+    dateEl.textContent = moment().format("MMMM Do, YYYY - hh:mm a");
+    var dateContainer = document.querySelector("#currentDay");
+    //console.log(dateEl.textContent);
+
+    //append dateEl to its parent container, dateContainer
+    dateContainer.appendChild(dateEl);
+};
+
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+function auditItems(itemEl) {
+        //if the item is in an hour in the past, it's white
+        if(moment().isBefore(time)) {
+            $(itemEl).addClass("past");
+        } else if (moment().isSame(time)) {
+        //if the item is in the current hour, it's red
+            $(itemEl).addClass("present");
+        //if the item is in a future hour, it's green
+        } else if (moment().isAfter(time)) {
+            $(itemEl).addClass("future");
+        }
+}
+
+
+// WHEN I click into a time block
+// THEN I can enter an event
+//item is entered into the textarea and the button is pressed to save the item
+function createItem() {
+    var itemToSave = document.getElementsByClassName(".textarea").value;
+    console.log(itemToSave);
+
+    for (var i = 0; i < saveButtonEl.length; i++) {
+        //add event listener for save button. items should be saved to the container
+        saveButtonEl.addEventListener("click", saveItem());
+        console.log("this worked");
+    }
+};
+
+
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+//saved user inputs added to localstorage
+function saveItem() {
+localStorage.setItem("name", JSON.stringify(items));
+
+console.log(savedItems);
+
+    //if there are no saved items, show nothing. if there are saved items, show them in their time blocks
+    if (savedItems === 0) {
+        savedItems.textContent = "";
+    } else {
+        localStorage.getItem("name");
+    }
+}
+
+showDate();
